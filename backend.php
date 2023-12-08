c793d09105dce9a36f14ebfc30cf69e32314aa74
Bahdanovich <bahdanovich@internet.ru>
2023-12-08 22:44:38 +0300

    Added back button

--- backend.php
+++ backend.php
@@ -15,10 +15,12 @@ $response = $router->handleRequest($requestType, $requestData);
 
 if (isset($response[0]['lat']) && isset($response[0]['lon'])) {
     echo 'latitude: ' . $response[0]['lat'] . '<br>' . 'longitude: ' . $response[0]['lon'];
+    echo '<br><button><a href="index.php">Back</a></button>';
 }
 
 if (isset($response['address']['city'])) {
     echo 'Address: ' . $response['address']['city'] . '<br>' . 'Country: ' . $response['address']['country'];
+    echo '<br><button><a href="index.php">Back</a></button>';
 }
 
 return json_encode($response);
